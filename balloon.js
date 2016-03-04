36cb4e16d8143a43f1f8e9ac38f2344cd5843149
Pooja Tandan <poojatandan@Poojas-MacBook-Pro.local>
2016-03-04 17:45:00 -0500

    project

--- balloon.js
+++ balloon.js
@@ -1,156 +1,194 @@
 
-// // window.onload = function() {
-//   // your code goes here
-//   var balloon = document.querySelector(".color");
-//   var left = 0;
-//   var direction = 'right';
-//
-//   var walkRight = setInterval(function() {
-//     balloon.style.left = left + "px";
-//     if (direction === "right") {
-//       if (left <= window.innerWidth - 100) {
-//         left += 10;
-//         console.log(left);
-//         if (left > 200) {
-//           left
-//         }
-//       } else {
-//         direction = 'left';
-//         balloon.className = 'left';
-//       }
-//     } else {
-//       if (left >=0) {
-//         left -= 10;
-//       } else {
-//         direction = 'right';
-//         balloon.className = '';
-//       }
-//     }
-//
-//   }, 50);
-// // };
-//
 
-var left = 0;
-var bottom = 0;
-var movement = "right";
-var upDown = "up";
-var rand = Math.round(Math.random() * 13 + 2);
-var walkRight = setInterval(function(){
- catHouse.style.left = left + "px";
- if(movement == "right"){
-  cat.style.height = '500px';
-  cat.style.width = '500px';
-  cat.style.transitionDuration = "1s";
-  if (left <= window.innerWidth - 300)
-   {   left += rand;    }
- else {  movement = "left";
- cat.style.height = '150px';
- cat.style.width = '150px';
- cat.style.transitionDuration = "1s";
- catHouse.className = "left";       }
-} else {
-  if (left >= 0)
-   {     left -= rand;
-   } else {
-    movement = "right";
-    catHouse.className = "right";}}},20);
-var walkRight = setInterval(function(){
- catHouse.style.bottom = bottom + "px";
- if(upDown == "up"){
-   if (bottom <= window.innerHeight - 250){
-     bottom += 10;
-   } else { upDown = "down"; }
- } else {
-   if (bottom >= 0){
-     bottom -= 10;
-   } else {  upDown = "up";}}},20);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var blue = document.querySelector('.color');
+var yellow = document.querySelector('.color');
 var orange = document.querySelector('.color1');
 var green= document.querySelector('.color2');
 var darkgreen=document.querySelector('.color3');
-var lightgreen=document.querySelector('color4');
-var button=document.querySelector('btn');
+var lightgreen=document.querySelector('.color4');
+var button=document.querySelector('.btn');
 var popCount = 0;
 
-blue.addEventListener('click', function(){
+
+// var opener = document.createElement('div');
+// opener.id = "opener";
+// opener.innerText = "Pop as many balloons as you can!";
+// document.body.appendChild(opener);
+// setTimeout(function() {
+//   opener.remove();
+// }, 1000);
+
+yellow.addEventListener('click', function(){
   alert("You Clicked Me!");
   popCount +=1;
-  blue.innerHTML="<img src='images/Balloon-psd91072.png', height='150px' width= '100px'>";
-
-
- //  $(".color").click(function(){
- // $(this).toggleClass("rotator")  ;
+  yellow.innerHTML="<img src='images/balloon-md.png', height='150px' width= '100px'>";
+  setTimeout(function () {
+    yellow.innerHTML="";
+  }, 1000);
+  addBalloon();
 });
 orange.addEventListener('click',function(){
   alert("Hoorah You Choose Me!");
   popCount +=1;
   orange.innerHTML="<img src='images/blue-balloon-hi.png', height='150px'width='100px'>";
+  setTimeout(function () {
+    orange.innerHTML="";
+  }, 1000);
+  addBalloon();
 });
   green.addEventListener('click',function(){
     alert("yeah!");
     green.innerHTML="<img src='images/popped.png', height='150px' width='150px'>";
-    popCount +=1;
     setTimeout(function () {
       green.innerHTML="";
     }, 1000);
-
-
+    addBalloon();
+popCount +=1;
 var audio = new Audio('http://soundbible.com/grab.php?id=1522&type=mp3');
     audio.play();
 });
 
 darkgreen.addEventListener('click',function() {
-  alert("Yippy Yippy");
-  darkgreen.innerHTML="<img src='images/margenta.png',height='250px'width=150px'>";
+  // alert("Yippy Yippy");
+  var bal =document.createElement('div');
+  bal.id="game";
+  bal.innerText="Hoorah";
+  document.body.appendChild(bal);
+  darkgreen.innerHTML="<img src='images/pink.png',height='250px'width=150px'>";
+  var audio = new Audio('http://soundbible.com/grab.php?id=1522&type=mp3');
+  audio.play();
   setTimeout(function() {
     darkgreen.innerHTML="";
-  },1000);
+  }, 1000);
+  addBalloon();
   popCount +=1;
 });
 
 lightgreen.addEventListener('click',function() {
   alert("Hey You Block Me");
-  lightgreen.innerHTML="<img src='images/Boom.png',height='250px'width=150px'>";
-  SetTimeout(function () {
-    lightgreen.innerHTML="";
+  lightgreen.innerHTML="<img src='images/boom.png',height='250px'width=150px'>";
+  setTimeout(function () {
+    lightgreen.remove();
   },1000);
+  addBalloon();
   popCount +=1;
 });
 button.addEventListener('click',function(){
     alert("clicked");
-    var audio = new Audio('http://soundbible.com/grab.php?id=1522&type=mp3');
-    audio.play();
-    popCount +=1;
+  var opener = document.createElement('div');
+  opener.id = "opener";
+  opener.innerText = "Pop as many balloons as you can I willchanged the color!";
+  document.body.appendChild(opener);
+  setTimeout(function() {
+    opener.remove();
+  }, 2000);
+  popCount +=1;
 });
 
 
+function addBalloon () {
+  var possibleColors = ['color', 'color1', 'color2', 'color3', 'color4'];
+
+  var newBalloon = document.createElement('div');
+  var colorChoice = possibleColors[Math.floor(Math.random() * possibleColors.length)];
+
+  newBalloon.classList.add(colorChoice);
+  if (colorChoice == possibleColors[0]) {
+    newBalloon.innerHTML = '<img src="images/yellow.png" height="150px" width="100px">';
+    newBalloon.addEventListener('click',function(){
+    newBalloon.remove();
+      addBalloon();
+    });
+  }
+  if (colorChoice == possibleColors[1]) {
+    newBalloon.innerHTML = '<img src="images/standard-05-orange.png">';
+    newBalloon.addEventListener('click',function(){
+    newBalloon.remove();
+      addBalloon();
+    });
+  }
+  if (colorChoice == possibleColors[2]) {
+    newBalloon.innerHTML = '<img src="images/large_balloons.png">';
+    newBalloon.addEventListener('click',function(){
+    newBalloon.remove();
+      addBalloon();
+
+    });
+  }
+  if (colorChoice == possibleColors[3]) {
+    newBalloon.innerHTML = '<img src="images/animated.gif">';
+    newBalloon.addEventListener('click',function(){
+    newBalloon.remove();
+      addBalloon();
+    });
+  }
+  if (colorChoice == possibleColors[4]) {
+    newBalloon.innerHTML = '<img src="images/large_cute.png">';
+    newBalloon.addEventListener('click',function(){
+      newBalloon.remove();
+      addBalloon();
+    });
+  }
 
 
-// changesource();
+  var maxWidth = window.innerWidth;
+  var maxHeight = window.innerHeight;
+
+  newBalloon.style.left = 100 + Math.floor(Math.random() * (maxWidth - 200)) + "px";
+  newBalloon.style.top = 100 + Math.floor(Math.random() * (maxHeight - 200)) + "px";
+
+  console.log(newBalloon);
+
+  document.body.appendChild(newBalloon);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var balloons = document.querySelectorAll('.balloon');
+// for (var i = 0; i < balloons.length; i++) {
+//   balloons[i].style.left=left + "px";
+// }
+//
+// // var bottom = 0;
+// // var movement = "right";
+// // var upDown = "up";
+// var rand = Math.round(Math.random() * 13 + 2);
+// var balloons = setInterval(function(){
+//  balloons.style.left = left + "px";
+//  if(movement == "right"){
+//   balloons.style.height = '500px';
+//   balloons.style.width = '500px';
+//   balloons.style.transitionDuration = "1s";
+//   if (left <= window.innerWidth - 300)
+//    {   left += rand;    }
+//  else {  movement = "left";
+//  balloons.style.height = '150px';
+//  balloons.style.width = '150px';
+//  balloons.style.transitionDuration = "1s";
+//  balloons.className = "left";       }
+// } else {
+//   if (left >= 0)
+//    {     left -= rand;
+//    } else {
+//     movement = "right";
+//     balloons.className = "right";}}},20);
+// var walkRight = setInterval(function(){
+//  balloons.style.bottom = bottom + "px";
+//  if(upDown == "up"){
+//    if (bottom <= window.innerHeight - 250){
+//      bottom += 10;
+//    } else { upDown = "down"; }
+//  } else {
+//    if (bottom >= 0){
+//      bottom -= 10;
+//    } else {  upDown = "up";}}},20);
